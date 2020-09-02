76b9abbfb0ea89fa65de92b8212258ad821080a3
SBastos800 <soniavsbastos@gmail.com>
2020-09-02 22:34:29 +0100

    iterated over each blog in app.js and display dinamic content in blogs page using ejs

--- app.js
+++ app.js
@@ -8,19 +8,24 @@ app.set('view engine', 'ejs');
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.render('index');
+    const blogs = [
+        {title: 'spaghetti bolognese recipe', snippet:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe, necessitatibus non. Dolorem, eius tempore. Obcaecati, sint. Iste aut harum quos ratione perferendis ea soluta quo sit quidem accusantium, commodi ducimus.'},
+        {title: 'a quick and delicious dinner', snippet:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe, necessitatibus non. Dolorem, eius tempore. Obcaecati, sint. Iste aut harum quos ratione perferendis ea soluta quo sit quidem accusantium, commodi ducimus.'},
+        {title: 'a veggie recipe to start the week', snippet:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe, necessitatibus non. Dolorem, eius tempore. Obcaecati, sint. Iste aut harum quos ratione perferendis ea soluta quo sit quidem accusantium, commodi ducimus.'}
+    ];
+    res.render('index', {title: 'Home', blogs: blogs});
 });
 
 app.get('/about', (req, res) => {
-    res.render('about');
+    res.render('about', {title: 'About'});
 });
 
 app.get('/blogs/create', (req, res) => {
-    res.render('create');
+    res.render('create', {title: 'Create a new blog'});
 })
 
 //404 page, it should be the last one 
 
 app.use((req, res) => {
-    res.status(404).render('404');
+    res.status(404).render('404', {title: '404 page not found'});
 });
