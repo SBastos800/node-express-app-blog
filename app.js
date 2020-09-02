b968186c3a017a98d447a2d9b16da15e67b61b44
SBastos800 <soniavsbastos@gmail.com>
2020-09-02 19:04:00 +0100

    built router

--- app.js
+++ app.js
@@ -5,5 +5,20 @@ const app = express();
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.send("hello");
-})
+    res.sendFile('./views/index.html', { root: __dirname});
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile('./views/about.html', { root: __dirname});
+});
+
+//redirect
+app.get('/about-us', (req, res) => {
+    res.redirect('/about');
+});
+
+//404 page, it should be the last one 
+
+app.use( (req, res) => {
+    res.status(404).sendFile('./views/404.html', { root: __dirname});
+});
