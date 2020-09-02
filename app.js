10a311441ea974dc3004e1b0b3bf76018d3782b0
SBastos800 <soniavsbastos@gmail.com>
2020-09-02 19:38:01 +0100

    set up view engine, replaced html files for ejs files, replaced sendFile for render()

--- app.js
+++ app.js
@@ -2,23 +2,25 @@ const express = require('express');
 
 const app = express();
 
+//setting up a view engine
+app.set('view engine', 'ejs');
+
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', { root: __dirname});
+    res.render('index');
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', { root: __dirname});
+    res.render('about');
 });
 
-//redirect
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
-});
+app.get('/blogs/create', (req, res) => {
+    res.render('create');
+})
 
 //404 page, it should be the last one 
 
-app.use( (req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname});
+app.use((req, res) => {
+    res.status(404).render('404');
 });
