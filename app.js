e40f88715811a561a8d1a8f1b0c228c52fb6b1a8
SBastos800 <soniavsbastos@gmail.com>
2020-09-03 17:12:44 +0100

    created a Mongo DB cluster, installed mongoose, connected mongoose

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+//connect to Mongo DB
+const dbURI = 'mongodb+srv://Blog-Kitchen:KitchenPractice2020@cluster0.yyidb.mongodb.net/<dbname>?retryWrites=true&w=majority';
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
 
 //setting up a view engine
 app.set('view engine', 'ejs');
 
-app.listen(3000);
 
 //static files
 app.use(express.static('public'));
